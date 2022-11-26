8485f41be41f3a4ec5640a77b28bb98efb8cc3cd
Alexander Nazimov <nasimov.alexander@gmail.com>
2022-11-27 00:18:23 +0500

    README file new text

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ HELP = """
     p - the command to search for the owner by document number,
     s - the command shows the shelf on which the document is stored,
     l - the command displays all the documents that are in the list,
-    a - the command adds information about the new document and its owner, and also adds the document to the desired shelf,
-    d - the command removes all information about the document, the owner from the list and removes the document from the shelf,
+    a - the command adds information about the new document and its owner, /
+    and also adds the document to the desired shelf,
+    d - the command removes all information about the document, the owner/
+     from the list and removes the document from the shelf,
     as - the command adds a new empty shelf,
     m - the command moves the document to another required shelf shelf
     h - the command will open the list of available commands again,
