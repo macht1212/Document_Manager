9cb0c055ff66376b5aca8671f16636871ff72f8d
Alexander Nazimov <nasimov.alexander@gmail.com>
2022-12-17 16:42:51 +0300

    data - added lict of commands, main - started Tkinter

--- data.py
+++ data.py
@@ -7,13 +7,11 @@ directories = {
 }
 
 HELP = """
-    p - the command to search for the owner by document number,
-    s - the command shows the shelf on which the document is stored,
-    l - the command displays all the documents that are in the list,
+    Print Doc info - the command to search for the owner by document number, 
+    List of Docs - the command displays all the documents that are in the list, 
     a - the command adds information about the new document and its owner, and also adds the document to the desired shelf,
     d - the command removes all information about the document, the owner from the list and removes the document from the shelf,
-    as - the command adds a new empty shelf,
-    m - the command moves the document to another required shelf shelf
-    h - the command will open the list of available commands again,
-    exit - the command will terminate the program.
+    h - the command will open the list of available commands again.
 """
+
+HELP_list = ['Print Doc info', 'List of Docs', 'Add Doc info', 'Delete Doc info', 'Open helper']
--- main.py
+++ main.py
@@ -1,36 +1,64 @@
 from Function import *
 from data import *
+import tkinter as tk
+from tkinter import ttk
+
+
+
+window = tk.Tk()
+window.title('Document manager')
+window.geometry('500x500')
+
+items = HELP_list
+
+frame_wrap = tk.Frame(window)
+frame_command = tk.Frame(frame_wrap, height=150, bg='green')
+frame_input_info = tk.Frame(frame_wrap, height=150, bg='yellow')
+
+
+frame_wrap.pack(fill='both')
+frame_command.pack(side='left', fill='both', expand=True, ipady=60)
+frame_input_info.pack(side='right', fill='both', expand=True, ipady=60)
+
+
+l_choose_command = ttk.Label(text='Choose command: ')
+f_choose_command = ttk.Combobox(values=items) 
+btn_submit = ttk.Button(text='Submit')
+
+window.mainloop()
+
+
 
 # В данной версии попробую сделать более юзабельный пользовательский интерфейс на основе встроенной библиотеки Tkinter 
 
 
-helper = input("Do you need a list of available features? (Y/N): ")
-if helper.lower() == "y":
-    print("The list of commands: \n", HELP)
+# helper = input("Do you need a list of available features? (Y/N): ")
+# if helper.lower() == "y":
+#     print("The list of commands: \n", HELP)
 
-while True:
-    command = input("Enter command: ").lower()
-    if command == "p":
-        people()
-    elif command == "s":
-        shelf()
-    elif command == "l":
-        list_doc()
-    elif command == "a":
-        add()
-    elif command == "d":
-        delete()
-    elif command == "as":
-        add_shelf()
-    elif command == "m":
-        print("Available shelves: \n")
-        for key in directories.keys():
-            print(key.capitalize())
-        print()
-        move()
-    elif command == "h":
-        print("The list of commands: \n", HELP)
-    elif command == "exit":
-        exit()
-    else:
-        print("There is no such command in the list")
+# while True:
+#     command = input("Enter command: ").lower()
+#     if command == "p":
+#         people()
+#     elif command == "s":
+#         shelf()
+#     elif command == "l":
+#         list_doc() 
+#     elif command == "a":
+#         add()
+#     elif command == "d":
+#         delete()
+#     elif command == "as":
+#         add_shelf()
+#     elif command == "m":
+#         print("Available shelves: \n")
+#         for key in directories.keys():
+#             print(key.capitalize())
+#         print()
+#         move()
+#     elif command == "h":
+#         print("The list of commands: \n", HELP)
+#     elif command == "exit":
+#         exit()
+#     else:
+#         print("There is no such command in the list")
